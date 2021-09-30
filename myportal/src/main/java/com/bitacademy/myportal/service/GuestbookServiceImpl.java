da1acf636ec9665cf2b4c5c6c222f9260c6d60d7
강호진 <battlemech@naver.com>
2021-09-30 13:47:23 +0900

    방명록 삭제

--- myportal/src/main/java/com/bitacademy/myportal/service/GuestbookServiceImpl.java
+++ myportal/src/main/java/com/bitacademy/myportal/service/GuestbookServiceImpl.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bitacademy.myportal.repository.GuestbookDao;
 import com.bitacademy.myportal.repository.GuestbookVo;
 
 @Service
+//	service -> 선택적. 지금 단계에서는 그냥 저장소랑 컨트롤의 연결다리 밖에 안 됨
+//	구조가 복잡해져서 여러 DAO를 이용하거나 하는 경우에 활약함
+//	해당 작업에 Transaction을 걸어줄 수도 있음
 public class GuestbookServiceImpl implements GuestbookService {
 	@Autowired
 	GuestbookDao guestbookDaoImpl;
@@ -19,16 +23,19 @@ public class GuestbookServiceImpl implements GuestbookService {
 		return list;
 	}
 
+	@Transactional
 	@Override
 	public boolean writeMessage(GuestbookVo vo) {
-		// TODO Auto-generated method stub
-		return false;
+		int insertedCount = guestbookDaoImpl.insert(vo);
+		System.out.println("삽입된 방명록 레코드:" + insertedCount);
+		return 1 == insertedCount;
 	}
 
 	@Override
 	public boolean deleteMessge(GuestbookVo vo) {
-		// TODO Auto-generated method stub
-		return false;
+		int deletedCount = guestbookDaoImpl.delete(vo);
+		System.out.println("삭제된 방명록 레코드:" + deletedCount);
+		return 1 == deletedCount;
 	}
 
 }
